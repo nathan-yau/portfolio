4e598a5894d6d3a2b9b5067de9dbd901af34b49c
nathan-yau <nshyau@gmail.com>
2023-08-29 22:09:29 -0700

    Fix image path

--- components/Experiences/EducationSection.tsx
+++ components/Experiences/EducationSection.tsx
@@ -5,11 +5,11 @@ export default function EducationSection() {
         <div className={styles.education}>
             <h2 className={styles.educationTitle}>Education</h2>
             <div className={styles.educationSection}>
-                <img src="/BCIT.png" width={175} />
+                <img src="/bcit.png" width={175} />
                 <span>Computer System Technology (CST) Dipolma</span>
             </div>
             <div className={styles.educationSection}>
-                <img src="/SFU.webp" width={155} />
+                <img src="/sfu.webp" width={155} />
                 <span>Bachelor of Business Administration</span>
             </div>
         </div>
