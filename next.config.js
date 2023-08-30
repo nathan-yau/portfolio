0dfa36d4071aac56c7ccdd0fa4408dd43c4da554
nathan-yau <nshyau@gmail.com>
2023-08-29 21:29:52 -0700

    Update js files for azure deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = async (phase, { defaultConfig }) => {
-    /**
-     * @type {import('next').NextConfig}
-     */
-    const nextConfig = {
-      output: 'standalone',
-    };
-  
-    return nextConfig;
-  };
+module.exports = {
+    output: "standalone",
+}
